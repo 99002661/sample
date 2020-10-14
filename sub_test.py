a577cdc99526e113ecbefbd1d7b74fccf01b8f2f
99002661 <71481321+99002661@users.noreply.github.com>
2020-10-14 12:08:37 +0530

    Update sub_test.py

--- sub_test.py
+++ sub_test.py
@@ -1,11 +1,11 @@
  
 import unittest
-from sub import sub
+from sub import subs
 # import sub
 
 class MyTest(unittest.TestCase):
     def test_my_function(self):
-        self.assertEqual(sub(8,7), 1)
+        self.assertEqual(subs(8,7), 1)
         
         
 if __name__ == '__main__':
