6719549d50bbf7daae7bf1461da39a654ab5f88e
99002661 <71481321+99002661@users.noreply.github.com>
2020-10-14 11:59:03 +0530

    Create testsub.py

---
+++ testsub.py
@@ -0,0 +1,12 @@
+ 
+import unittest
+from sub import sub
+# import sub
+
+class MyTest(unittest.TestCase):
+    def test_my_function(self):
+        self.assertEqual(sub(8,7), 1)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
